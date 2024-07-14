20861ff4f5ec4b30effe36f782b6a8a59bafeeff
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2024-07-14 21:47:13 +0530

    changes

--- FirstJavaProject/src/App.java
+++ FirstJavaProject/src/App.java
@@ -1,5 +1,13 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Car car1=new Car();
+        car1.run();
+        Car car2=new Car();
+        car2.run();
+
+
     }
 }
