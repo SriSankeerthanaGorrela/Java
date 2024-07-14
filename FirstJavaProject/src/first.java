8d6d9c66bcb29a9ecf2abe9dd45a1724d3bb8a77
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2024-07-14 16:39:05 +0530

    update

---
+++ FirstJavaProject/src/first.java
@@ -0,0 +1,14 @@
+public class first {
+    public static void main(String[] args) {
+        int Count=10;
+        System.out.println("count value"+Count);
+    
+        Count=20;
+        System.out.println("count value "+Count);
+        double interest=5.8;
+        String name="sanky";
+        float interestf=5.8f;
+        System.out.println("Interest rate is " +interestf);
+}
+    
+}
